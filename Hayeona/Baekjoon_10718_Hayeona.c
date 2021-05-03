1e06e72763a48d060ff8ed7137d56103c9d55573
vancomycin-C <83212664+vancomycin-C@users.noreply.github.com>
2021-05-03 18:29:22 +0900

    Add files via upload

---
+++ Hayeona/Baekjoon_10718_Hayeona.c
@@ -0,0 +1,9 @@
+//문제 번호_10718_We love kriii
+#include<stdio.h>
+int main()
+{
+	printf("강한친구 대한육군\n");
+	printf("강한친구 대한육군");
+
+	return 0;
+}
---
+++ Hayeona/CodeCure_2021_4_Hayeona.c
@@ -0,0 +1,8 @@
+//CodeCure 2021 지원 서류 문제 4번_당신의 이름과 학과를 출력하시오.
+#include<stdio.h>
+int main() 
+{
+	printf("하연아 \n소프트웨어학과");
+
+	return 0;
+}
